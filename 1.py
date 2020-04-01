5f8cd440e9c3ff09b6fecaaf664c4624f28a2ba6
alexandruchitic <mr.alexchitic@gmail.com>
2020-04-01 21:02:20 +0200

    lesson 1

---
+++ 1.py
@@ -0,0 +1,9 @@
+character_name = "Alex"
+character_age = "30.23243234"
+is_male = True
+
+phrase = "Salut" " " + character_name + ", ce faci cuae?\nCum e sa o arzi la " + character_age + " de ani ?"
+
+print(phrase.upper().isupper())
+
+print(len(phrase))
