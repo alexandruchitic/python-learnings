ba28fda67607bde56fd62c38188522912b919c2b
alexandruchitic <mr.alexchitic@gmail.com>
2020-04-18 14:54:57 +0200

    no idea

--- 1.py
+++ 1.py
@@ -7,3 +7,14 @@ phrase = "Salut" " " + character_name + ", ce faci cuae?\nCum e sa o arzi la " +
 print(phrase.upper().isupper())
 
 print(len(phrase))
+
+
+
+#operators
+
+x = 5 
+y = 10
+
+multiplication_result = x + y
+
+print(multiplication_result)
