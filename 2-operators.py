64529f44e192bc3d0796597ee06aa08deac3a670
alexandruchitic <mr.alexchitic@gmail.com>
2020-04-20 21:45:53 +0200

    math imports

--- 2-operators.py
+++ 2-operators.py
@@ -1,19 +1,19 @@
+from math import *
+
 x = 10
 y = 4
 
 #simple if & print
-if x < y: print(x*y)
 
+my_num = x * y
+
+if x > y:
+        print("x e mai mare ca y si inmultirea lor e " + str(my_num))
 
 #less than or equal to
-if x <= y: print(x+y)
+if x >= y:
+	print(x+y)
 
-#identity operators (is / is not)
-if x is 10 and y is 4:
-    print("x e" x  "si y e" y)
-#elif - if you have more than two options to tell apart using the conditional operator
-#you can use if... elif.. else statement:
-elif x > y:
-    print("x e mai mare decat y")
-else:
-    print("nu se aplica nimic")
+result = pow(5,23)
+
+print("4 la puterea a 6-a e " + str(result))
