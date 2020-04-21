8d0d97cbb55273d8487764c6fcf3572485c10a4e
alexandruchitic <mr.alexchitic@gmail.com>
2020-04-21 21:26:49 +0200

    more excercises

---
+++ 4-calculator.py
@@ -0,0 +1,6 @@
+num1 = input("enter a number: ")
+num2 = input("Enter a second number: ")
+
+result = float(num1) + float(num2)
+
+print(result)
---
+++ 5-madlibs-game.py
@@ -0,0 +1,10 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input ("Enter a celebrity: ")
+
+
+print("roses are " + color)
+print(plural_noun +  " are blue")
+print("I love " + celebrity)
+
+
